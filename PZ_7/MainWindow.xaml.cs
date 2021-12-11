b6fbdc25993a6753ff1880ee5d4675bd31cde807
Денис Григорьев <Denisgri25@outlook.com>
2021-12-11 03:37:40 +0300

    Add project files.

---
+++ PZ_7/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PZ_7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly List<Student> _students = Student.StudentList;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<Student>? ReadFromFile()
+        {
+            return null;
+        }
+
+        void WriteToFile(string path, List<Student> students)
+        {
+
+        }
+
+        string FindElement(string criterion)
+        {
+            switch (criterion)
+            {
+                default:
+                {
+                    break;
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private void AddStudent_OnClick(object sender, RoutedEventArgs e)
+        {
+            Student student = new Student("Kent C.K.", "55555", new []{6,7,8,9,10});
+            Student.AddStudent(student);
+        }
+    }
+}
---
+++ PZ_7/Students.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Documents;
+
+namespace PZ_7
+{
+    public readonly struct Student
+    {
+        private readonly string _fio;
+        private readonly string _group;
+        private readonly int[] _scores = new int[5];
+
+        public Student(string fio, string group, int[] scores)
+        {
+            _fio = fio;
+            _group = group;
+            _scores = scores;
+        }
+
+        public string? GetFio => _fio;
+        public string? GetGroup => _group;
+        public string? GetScores => string.Join(",", _scores);
+
+        public static List<Student> StudentList { get; set; } = GetStudents();
+
+        public static List<Student> GetStudents()
+        {
+            List<Student> ss = new List<Student>();
+            Student student1 = new Student("Kent C.K.", "55555", new[] {6, 7, 8, 9, 10});
+            Student student2 = new Student("Kent K.C.", "12345", new[] {1, 2, 3, 4, 5});
+            ss.Add(student1);
+            ss.Add(student2);
+            ss.Add(student1);
+            ss.Add(student1);
+            ss.Add(student2);
+            StudentList = ss;
+            return ss;
+        }
+
+        public static List<Student> AddStudent(Student student)
+        {
+            StudentList.Add(student);
+
+            return StudentList;
+        }
+
+        public List<Student> ShowHighScoreStudents(List<Student> rawList)
+        {
+            var highScoreStudents = new List<Student>();
+            if (highScoreStudents == null) throw new ArgumentNullException(nameof(highScoreStudents));
+            highScoreStudents.AddRange(rawList.Where(student => student._scores.Any(score => score >= 9)));
+
+            return highScoreStudents;
+        }
+    }
+}
